9336e7e6433511b8e7f987cea33309f0b5fb3e1d
changwook lee <lcy12225@naver.com>
2023-12-03 14:49:58 +0900

    phone 유효성검사 추가

--- src/main/java/com/ft/favoritethings/member/dto/request/MemberRequestDto.java
+++ src/main/java/com/ft/favoritethings/member/dto/request/MemberRequestDto.java
@@ -20,6 +20,8 @@ public class MemberRequestDto {
     @Size(min = 2, max = 8, message = "닉네임은 2자 이상 8자 이하로 생성해주세요.")
     @Pattern(regexp = "^[가-힣a-zA-Z]+$", message = "닉네임에는 한글과 영문자만 사용할 수 있습니다.")
     private String nickname;
+
+    @Pattern(regexp = "^010-\\d{4}-\\d{4}$", message = "전화번호는 010-0000-0000 형식으로 입력해주세요.")
     private String phone;
 
     @Pattern(regexp = PASSWORD_PATTERN, message = "비밀번호는 6자 이상 소문자와 특수문자를 섞어서 생성해주세요.")
