f8fdd8ba9ae2b16ae198af10c20a9d5326f109d2
hoyaa <hoyaa@bigc.im>
2024-01-02 15:04:23 +0900

    spot CRUD 수정

--- src/main/java/com/ft/favoritethings/spot/repository/SpotRepository.java
+++ src/main/java/com/ft/favoritethings/spot/repository/SpotRepository.java
@@ -3,9 +3,5 @@ package com.ft.favoritethings.spot.repository;
 import com.ft.favoritethings.spot.entity.Spot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface SpotRepository extends JpaRepository<Spot, Long> {
-    @Override
-    Optional<Spot> findById(Long aLong);
 }
